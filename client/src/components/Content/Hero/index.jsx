ea8d516ffeb70ade20f0b28e36e362ef6785c994
ThStranick <ThStranick@gmail.com>
2024-07-16 11:57:50 -0400

    styling

--- client/src/components/Content/Hero/index.jsx
+++ client/src/components/Content/Hero/index.jsx
@@ -4,7 +4,7 @@ import ActionCardRight from "./components/ActionCardRight"
 export default function Hero(){
     return(
         <section id="hero">
-            <h1>This site is about birds of the Northeast! You can checkout the list of common birds and test yourself on identification.</h1>
+            <h1>This site is about birds of New Jersey! You can checkout the list of common birds or test yourself on identification.</h1>
             <section id="cards">
                 <ActionCardLeft/>
                 <ActionCardRight/>
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return(
         <footer>
-            <p> &copy;  Copyright 2024</p>
+            <p> &copy; Copyright 2024</p>
             <p>-Thomas Stranick</p>
         </footer>
     )
