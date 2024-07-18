b9b7002d303000d2d6d862a850af0808906bb2c2
ThStranick <ThStranick@gmail.com>
2024-07-18 10:29:12 -0400

    game logic

--- client/src/components/Content/Practice/components/Question.jsx
+++ client/src/components/Content/Practice/components/Question.jsx
@@ -2,21 +2,27 @@ import { useState } from "react"
 
 export default function Question(props){
     const [answer, setAnswer] = useState('')
+    const [wrongAnswerCount, setWrongAnswerCount] = useState(0) //waits 3 wrong answers before showing the image
 
     function handleSubmit(e){
         e.preventDefault()
         if(props.name === answer){
             console.log("Nice job!")
+            setWrongAnswerCount(0)
+            setTimeout(()=>{ //wait 3 sec before going to next question
+                props.nextQuestion()
+            }, "3000")
             return
         }
+        setWrongAnswerCount(wrongAnswerCount + 1)
         console.log("That's not it.")
     }
 
     return(
         <section id="question">
             <p>{props.text}</p>
-            <img src={props.img} alt="Img" />
             <audio controls src={props.call}></audio>
+            {wrongAnswerCount > 2 && <img src={props.img}/>} 
             <form onSubmit={handleSubmit}>
                 <label>Answer:</label>
                 <input type="text" onChange={(e)=>setAnswer(e.target.value)}/>
--- client/src/components/Content/Practice/index.jsx
+++ client/src/components/Content/Practice/index.jsx
@@ -1,6 +1,6 @@
 import Question from "./components/Question"
 import { useStore } from "../../../store";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const questions = [
     {
@@ -13,35 +13,77 @@ const questions = [
         name: "Blue Jay",
         img: "/Birds/Images/Blue-Jay.jpg",
         call:"/Birds/Calls/BLUJAY_1.jaycallsampclicks_FLle_1.mp3",
-        text: "This is the Blue Jay"
+        text: "What bird is this?"
     },
     {
         name: "Northern Cardinal",
         img: "/Birds/Images/Male-Northern-Cardinal.jpg",
         call:"/Birds/Calls/NORCAR_1.songsnum1_NYle_1.mp3",
-        text: "This is the Northern Cardinal"
+        text: "What bird is this?"
     },
     {
         name: "Mourning Dove",
         img: "/Birds/Images/Mourning-Dove.jpg",
         call:"/Birds/Calls/MOUDOV_1.cooamppartialcoo_NYle_1.mp3",
-        text: "This is the Mourning Dove"
+        text: "What bird is this?"
     }
 ]
 
 export default function Practice(){
     const {state} = useStore();
-    const [showStart, setShowStart] = useState(false)
+    const [questionArray, setQuestionArray] = useState(questions)
+    const [questionsLeft, setQuestionsLeft] = useState(questions.length)
+    const [showStart, setShowStart] = useState(true)
     const [showQuestions, setShowQuestions] = useState(false)
+    const [showEnd, setShowEnd] = useState(false)
+    const [questionNumber, setQuestionNumber] = useState(0)
+    const [nextQuestion, setNextQuestion] = useState(false)
+    const [questionArrayNumber, setQuestionArrayNumber] = useState(0)
+
+    useEffect(()=>{
+        if(showStart){ //if start screen is shown reset the question number
+            setQuestionNumber(0)
+        }
+        else{ //if turned off, start questions
+            setQuestionNumber(1)
+            getNextQuestion()
+        }
+    },[showStart])
+
+    useEffect(()=>{
+        if(nextQuestion){ 
+            if(questionsLeft > 0){
+                getNextQuestion()
+                setQuestionArray(old => {return old.filter((_,i)=> i !== questionArrayNumber)})
+            }
+            else {
+                setShowEnd(true)
+                setShowQuestions(false)
+            }
+            setNextQuestion(false)
+        }
+    },[nextQuestion])
+
+    const triggerNextQuestion = () => {
+        setQuestionNumber(questionNumber + 1) //triggers addition of question number when answer is clicked
+        setQuestionsLeft(questionsLeft - 1)
+        setNextQuestion(true)
+    }
+
+    const getNextQuestion = () => {
+        setQuestionArrayNumber(Math.floor(Math.random() * questionsLeft))
+        console.log(questionsLeft)
+    }
 
     function handleBegin(){
         setShowStart(false)
         setShowQuestions(true)
     }
 
-    function handleEnd(){
+    function handleBack(){
         setShowStart(true)
         setShowQuestions(false)
+        setShowEnd(false)
     }
     
     return(
@@ -51,9 +93,14 @@ export default function Practice(){
                 <a onClick={handleBegin}>Begin</a>
             </section>}
             { showQuestions && <section id="question-card">
-                <h2>Question #</h2>
-                <Question name={questions[state.question].name} text={questions[state.question].text} img={questions[state.question].img} call={questions[state.question].call}/>
-                <a onClick={handleEnd}>Back</a>
+                <h2>Question {questionNumber}</h2>
+                <Question name={questionArray[questionArrayNumber].name} text={questionArray[questionArrayNumber].text} img={questionArray[questionArrayNumber].img} 
+                call={questionArray[questionArrayNumber].call} nextQuestion={triggerNextQuestion}/>
+                <a onClick={handleBack}>Back</a>
+            </section>}
+            {   showEnd && <section id="end-card">
+                <h2>You've completed the practice!</h2>
+                <a onClick={handleBack}>Practice Again</a>
             </section>}
         </section>
         
